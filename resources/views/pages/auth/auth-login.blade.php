ae6532ba14d1e56ed1138419d9349d790b88c328
ekosaputro039 <saputrae78@gmail.com>
2023-09-28 14:23:00 +0700

    02-fortify

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -16,21 +16,25 @@
 
         <div class="card-body">
             <form method="POST"
-                action="#"
+                action="{{ route('login')}}"
                 class="needs-validation"
                 novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email')
+                        ->is invalid
+                        @enderror"
                         name="email"
                         tabindex="1"
-                        required
                         autofocus>
+                    @error('email')
                     <div class="invalid-feedback">
-                        Please fill in your email
+                        {{ $message}}
                     </div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
@@ -46,13 +50,17 @@
                     </div>
                     <input id="password"
                         type="password"
-                        class="form-control"
+                        class="form-control @error('password')
+                            ->is invalid
+                        @enderror"
                         name="password"
                         tabindex="2"
                         required>
+                    @error('password')
                     <div class="invalid-feedback">
-                        please fill in your password
+                        {{ $message }}
                     </div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
@@ -75,21 +83,6 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
 
         </div>
     </div>
